09b61b783373e19b120fe5ca9de650f2499f3838
PriyankaBawanna <PriyankaBawanna@CESIT-LAP-0124>
2022-12-08 21:21:12 +0530

    added contact page

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "../header/header";
 import "./contact.css";
+import { RiCommunityFill, RiMailSendFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { allRoutes } from "../constant/path";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
 function Contact() {
   return (
     <>
@@ -9,6 +14,49 @@ function Contact() {
         <div className="address-section">
           <div className="address">
             <b>Address</b>
+          </div>
+          <div className="address-icon">
+            <RiCommunityFill />
+            <RiMailSendFill />
+          </div>
+        </div>
+        <div className="address-section">
+          <div className="address">
+            <b>Quick Links</b>
+          </div>
+          <div className="address-icon">
+            <Link className="about-us" to={allRoutes.about}>
+              About Us
+            </Link>
+          </div>
+        </div>
+
+        <div className="address-section">
+          <div className="address">
+            <b>Popular Links</b>
+          </div>
+          <div className="address-icon">
+            <Link className="about-us" to={allRoutes.about}>
+              About Us
+            </Link>
+          </div>
+        </div>
+
+        <div className="address-section">
+          <div className="address">
+            <b>Singups</b>
+          </div>
+          <div className="social-media-icon">
+            <div>
+              <a href="https://github.com/">
+                <AiOutlineGithub />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.facebook.com/">
+                <FaFacebookF />
+              </a>
+            </div>
           </div>
         </div>
       </div>
